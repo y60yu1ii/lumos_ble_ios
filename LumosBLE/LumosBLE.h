ac13cf662a784e27e850ef2edc81698ec4368bb2
yyl <y60yu1ii@gmail.com>
2019-02-11 16:33:49 +0800

    Initial Commit

---
+++ LumosBLE/LumosBLE.h
@@ -0,0 +1,19 @@
+//
+//  LumosBLE.h
+//  LumosBLE
+//
+//  Created by yaoyu on 2019/2/11.
+//  Copyright © 2019 fishare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LumosBLE.
+FOUNDATION_EXPORT double LumosBLEVersionNumber;
+
+//! Project version string for LumosBLE.
+FOUNDATION_EXPORT const unsigned char LumosBLEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LumosBLE/PublicHeader.h>
+
+
